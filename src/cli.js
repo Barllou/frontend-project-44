4b647dbd5165e9780f82b517db49b13daf23b471
Danil Tuzhik <danil-tuzhik@yandex.ru>
2022-11-30 21:59:55 +0700

    add src/ and add cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ let readlineSync = require('readline-sync');
 
 const getUserName = () => {
     let userName = readlineSync.question('May I have your name? ');
-    return `Hello ${userName}!`
+    return `Hello ${userName}!`;
 }
 
 module.exports.getUserName = getUserName;
