fef45bb44ba8fc83197ec25ad9cf06dec5db4880
Danil Tuzhik <danil-tuzhik@yandex.ru>
2022-12-01 22:28:31 +0700

    update cli.js and gameEven.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,8 @@ const readlineSync = require('readline-sync');
 
 const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  return `Hello ${userName}!`;
+  console.log(`Hello ${userName}!`);
+  return userName;
 };
 
 module.exports.getUserName = getUserName;
--- src/gameEven.js
+++ src/gameEven.js
@@ -1,14 +1,24 @@
 const readlineSync = require('readline-sync');
 
-const isFindEvenNumber = () => {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+const isFindEvenNumber = (nameUser) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(`Question: ${randomNumber}`);
-  const userResponse = readlineSync.question('Your answer: ');
-  const parity = randomNumber % 2 === 0 ? 'yes' : 'no';
-  if (userResponse !== 'yes' && userResponse !== 'no') {
-    return 'Let\'s try again';
+
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${randomNumber}`);
+    const userResponse = readlineSync.question('Your answer: ');
+    const parity = randomNumber % 2 === 0 ? 'yes' : 'no';
+
+    if (userResponse !== 'yes' && userResponse !== 'no') {
+      return `Let's try again, ${nameUser}!`;
+    }
+    if (parity === userResponse) {
+      console.log('Correct!');
+    } else {
+      return `Let's try again, ${nameUser}!`;
+    }
   }
-  return parity === userResponse ? 'Correct!' : 'Let\'s try again';
+  return `Congratulations, ${nameUser}!`;
 };
+
 module.exports.isFindEvenNumber = isFindEvenNumber;
