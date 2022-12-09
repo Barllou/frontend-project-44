2611b59e0ed6e55f0ccbf7cc002206060c30e510
Danil Tuzhik <danil-tuzhik@yandex.ru>
2022-12-09 23:21:07 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import readlineSync from 'readline-sync';
 
 // Общая логика игры
-const gamesLogic = (rulesGame, uniqLogic, NUMBER_ROUND = 3) => {
+const gamesLogic = (rulesGame, uniqLogic, NUM_ROUND = 3) => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}! \n${rulesGame}`);
 
-  for (let i = 1; i <= NUMBER_ROUND; i += 1) {
+  for (let i = 1; i <= NUM_ROUND; i += 1) {
     const uniqLogicGame = uniqLogic();
     const question = uniqLogicGame[0];
     const uniqResult = uniqLogicGame[1];
@@ -21,7 +21,7 @@ const gamesLogic = (rulesGame, uniqLogic, NUMBER_ROUND = 3) => {
       console.log(`${userResponse} is wrong answer ;(. Correct answer was '${uniqResult}. \nLet's try again, ${name}!`);
       break;
     }
-    if (i === NUMBER_ROUND) {
+    if (i === NUM_ROUND) {
       console.log(`Congratulations, ${name}!`);
     }
   }
