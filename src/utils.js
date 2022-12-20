64a322974d3ffe4e8a84776324c47205e325f940
Danil Tuzhik <danil-tuzhik@yandex.ru>
2022-12-20 19:42:40 +0700

    added helper function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
 const getRandomNumber = (end = 100) => Math.floor(Math.random() * end) + 1;
 
-export default getRandomNumber;
+// Get a random number from the selected range.
+const getRandomNumberFromRange = (min, max) => {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+};
+
+export { getRandomNumber, getRandomNumberFromRange };
