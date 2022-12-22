6786c0d41746033f964c0e68dbc94572c15c5441
Danil Tuzhik <danil-tuzhik@yandex.ru>
2022-12-22 23:43:01 +0700

    created an abstraction with a function

--- src/games/gameEven.js
+++ src/games/gameEven.js
@@ -1,12 +1,14 @@
 import { runGamesLogic } from '../../index.js';
 import { getRandomNumber } from '../utils.js';
 
+const isEven = (number) => (number % 2 === 0);
+
 const runFindEvenNumber = () => {
   const generateUniqGameData = () => {
     const MAX_NUM_GENERATE = 10;
     const randomValue = getRandomNumber(MAX_NUM_GENERATE);
     const question = randomValue;
-    const parity = randomValue % 2 === 0 ? 'yes' : 'no';
+    const parity = isEven(randomValue) ? 'yes' : 'no';
     return [question, parity];
   };
   const rulesGame = 'Answer "yes" if the number is even, otherwise answer "no".';
